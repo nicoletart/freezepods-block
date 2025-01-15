12dc08a6558523c9c0a29b9bd1b5f55bc0bde347
“Nicole <nikkat311@gmail.com>
2025-01-15 01:59:40 -0600

    Change welcome message

--- freezepods-block/src/app/page.js
+++ freezepods-block/src/app/page.js
@@ -2,10 +2,25 @@ import Navigation from "./components/Navigation";
 
 export default function Home() {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Navigation />
-      <div className="welcome-content">
+      <div
+        className="welcome-content"
+        style={{ textAlign: "center", maxWidth: "500px", gap: "20px" }}
+      >
         <h1>Welcome to Freezepods</h1>
+        <p>
+          Freezepods is an interactive platform where students are able to
+          design and build custom games using Blockly and Micro:bits. The
+          application provides a fun and educational way to learn coding while
+          interacting with new electronic devices.
+        </p>
       </div>
     </div>
   );
